5fd71298d649420f295e65cf1ad3a75b963155be
Talha Tallat <talhatallat0@gmail.com>
2022-10-24 16:17:57 +0100

    listing functions

---
+++ Python Programming/Chapter 3 - Lists/cars.py
@@ -0,0 +1,13 @@
+
+  cars = ['bmw', 'audi', 'toyota', 'subaru']
+  cars.sort()  #  sorts in alphabetical order
+  print(sorted(cars))  # sorting a List Temporarily with the sorted() Function
+  cars.sort(reverse=True) # sorts in reverse alphabetical order permanently
+  cars.reverse() # Reverces the List Order BUT doesn’t sort backward alphabetically!!
+  print(len(cars)) # prints the Length of a List
+  print(cars)
+
+
+
+
+
