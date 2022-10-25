c9d33531408d6bfc2cd8238d7a76d7a859b35d64
Talha Tallat <talhatallat0@gmail.com>
2022-10-25 14:42:17 +0100

    Create pizzas.py

---
+++ Python Programming/Chapter 4 - working with lists/pizzas.py
@@ -0,0 +1,14 @@
+pizzas = ['margerita', 'vegiterian', 'pepperoni'] # 4-1 Store these pizza names in a list 
+for pizza in pizzas:
+    print("I like " + pizza.title() + " pizza!")#4-1.1
+    #print("I can't wait to see your next trick, " + pizza.title() + ".\n")
+    
+print("\nI really love pizza!\n\n") # 4-1.2
+
+
+animals = ['dog', 'cat', 'rabbit'] # 4-2 Storing these animal names in a list 
+for animal in animals:
+    print("A " + animal.title() + " would make a great pet.")#4-2.1
+    #print("I can't wait to see your next trick, " + pizza.title() + ".\n")
+    
+print("\nAny of these animals would make a great pet!") # 4-1.2
