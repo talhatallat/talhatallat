9c5c58323264fefaae498cb1212ce0856e0ece04
Talha Tallat <talhatallat0@gmail.com>
2022-10-25 14:24:53 +0100

    Create magicians.py

---
+++ Python Programming/Chapter 4 - working with lists/magicians.py
@@ -0,0 +1,6 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians: # for loop ->  tells Python to retrieve the first value from the list magicians and store it in the variable magician. 
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+    
+print("Thank you, everyone. That was a great magic show!")
