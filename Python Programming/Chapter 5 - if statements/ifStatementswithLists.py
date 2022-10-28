4366c065f0719900e8db4fbb917447325c07ce28
Talha Tallat <talhatallat0@gmail.com>
2022-10-28 13:43:23 +0100

    in progress

---
+++ Python Programming/Chapter 5 - if statements/ifStatementswithLists.py
@@ -0,0 +1,15 @@
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+print("\nFinished making your pizza!")
+
+# 5-8. Hello Admin: Make a list of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the list, and print a greeting to each user:
+# • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+# • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
