dcc8b0961e2e08fba75f20378a19ef2096f2942f
Talha Tallat <talhatallat0@gmail.com>
2022-11-12 10:06:05 +0000

    Update even_or_odd.py

--- Python Programming/Chapter 7 - User Input and While Loop/even_or_odd.py
+++ Python Programming/Chapter 7 - User Input and While Loop/even_or_odd.py
@@ -1,6 +1,7 @@
 
 number = input("Enter a number, and I'll tell you if it's even or odd: ")
 number = int(number)
+# Even numbers are always divisible by two, so if the modulo of a number and two is zero (here, if number % 2 == 0) the number is even. Otherwise, it’s odd.
 if number % 2 == 0:
   print("\nThe number " + str(number) + " is even.")
 else:
