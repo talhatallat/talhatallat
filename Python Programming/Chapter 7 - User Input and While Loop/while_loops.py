78318f13bf90bc65e49f04680ec6c07a33b752de
Talha Tallat <talhatallat0@gmail.com>
2022-11-13 12:52:24 +0000

    Uploading three exit while loops

--- Python Programming/Chapter 7 - User Input and While Loop/while_loops.py
+++ Python Programming/Chapter 7 - User Input and While Loop/while_loops.py
@@ -8,6 +8,7 @@ while active:
         print("  I'll add " + topping + " to your pizza.")
     else:
         break
+        
 # 7-5. Movie Tickets: A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop in which you ask users their age, and then tell them the cost of their movie ticket.
 prompt = "\nTo find out your price for the ticket, please enter your age: "
 prompt += "\nEnter 'quit' when you are finished: "
@@ -25,15 +26,37 @@ while True:
         print("The ticket is $15.")
     else:
         break
-
-#7-6. Three Exits: Write different versions of either Exercise 7-4 or Exercise 7-5 that do each of the following at least once:
+        
+#7-6. Three Exits: Write different versions of either Exercise 7-5 that do each of the following at least once:
+prompt = "\nTo find out your price for the ticket, please enter your age: "
+prompt += "\nEnter 'quit' when you are finished: "
 #• Use a conditional test in the while statement to stop the loop.
+age = ""
+while age != 'quit':
+     age = input(prompt)
+     print(age)
 #• Use an active variable to control how long the loop runs.
+active = True
+while active:
+    age = input(prompt)
+    if age == 'quit':
+         active = False
+    else:
+        print(age) 
 #• Use a break statement to exit the loop when the user enters a 'quit' value.
-
-
-
-
-
-
+while True:
+    age = input(prompt)
+    if age == 'quit':
+        break
+    else:
+        print(age)
+        
 #7-7. Infinity: Write a loop that never ends, and run it. (To end the loop, press CTRL-C or close the window displaying the output.)
+while True:
+        print("never ends")
+        
+# good example of Infinite Loops
+#x = 1
+#while x <= 5:
+    #print(x)
+    #x += 1
