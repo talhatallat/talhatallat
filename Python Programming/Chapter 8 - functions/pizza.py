77471449cc579ec9432476153846073219648fe5
Talha Tallat <talhatallat0@gmail.com>
2022-11-22 22:40:31 +0000

    Update pizza.py

--- Python Programming/Chapter 8 - functions/pizza.py
+++ Python Programming/Chapter 8 - functions/pizza.py
@@ -1,8 +1,12 @@
 # Passing an Arbitrary Number of Arguments
 
+# The asterisk in the parameter name *toppings tells Python to make an empty tuple called toppings and pack whatever values it receives into this tuple.
+
 def make_pizza(*toppings):
-    """Print the list of toppings that have been requested."""
-    print(toppings)
-    
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+        
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
