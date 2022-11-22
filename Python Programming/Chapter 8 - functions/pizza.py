f386f88db659cfd3d74f217bdf59152353acd48f
Talha Tallat <talhatallat0@gmail.com>
2022-11-22 22:47:13 +0000

    Update pizza.py

--- Python Programming/Chapter 8 - functions/pizza.py
+++ Python Programming/Chapter 8 - functions/pizza.py
@@ -10,3 +10,15 @@ def make_pizza(*toppings):
         
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+# Mixing Positional and Arbitrary Arguments
+
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+        
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
