61e4de77297b2e74e1ec79e63ad0ea342ecb3d06
Talha Tallat <talhatallat0@gmail.com>
2023-01-05 14:03:13 +0000

    Create file_paths.py

---
+++ Python Programming/Chapter 10 - Files and Exceptions/Reading from a File/file_paths.py
@@ -0,0 +1,6 @@
+file_path = '/home/ehmatthes/other_files/text_files/filename.txt' #  On Linux and OS X, you use a  forward slash (/)
+with open(file_path) as file_object: 
+
+  
+file_path = 'C:\Users\ehmatthes\other_files\text_files\filename.txt' #  On Windows systems, you use a backslash (\)
+with open(file_path) as file_object: 
