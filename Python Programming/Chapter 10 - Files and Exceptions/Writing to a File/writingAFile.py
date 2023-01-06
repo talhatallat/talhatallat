4f30a01e5eed506de9d6240c554a50728b7f50a0
Talha Tallat <talhatallat0@gmail.com>
2023-01-06 22:10:00 +0000

    Update writingAFile.py

--- Python Programming/Chapter 10 - Files and Exceptions/Writing to a File/writingAFile.py
+++ Python Programming/Chapter 10 - Files and Exceptions/Writing to a File/writingAFile.py
@@ -6,3 +6,9 @@ with open(filename, w) as file_object:
     file_object.write(ask_user_name)
     
 # 10-4. Guest Book: Write a while loop that prompts users for their name. When they enter their name, print a greeting to the screen and add a line recording their visit in a file called guest_book.txt. Make sure each entry appears on a new line in the file.
+with open(filename, w) as file_object:
+    while True:
+        if ask_user_name = input("My name is: \n"):
+            print("Welcome " + ask_user_name +"!")
+        file_object.write(ask_user_name)
+    break;
