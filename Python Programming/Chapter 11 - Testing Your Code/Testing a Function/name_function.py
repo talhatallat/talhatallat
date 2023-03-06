6071cce5ea9922bc87b4ede42fc15fa86a0c8375
Talha Tallat <talhatallat0@gmail.com>
2023-03-06 14:55:15 +0000

    Update name_function.py

--- Python Programming/Chapter 11 - Testing Your Code/Testing a Function/name_function.py
+++ Python Programming/Chapter 11 - Testing Your Code/Testing a Function/name_function.py
@@ -1,4 +1,5 @@
-def get_formatted_name(first, last):    
+#  get_formatted_name() function combines the first and last name with a space in between to complete a full name, and then capitalizes and returns the full name
+def get_formatted_name(first, last):   
   """Generate a neatly formatted full name."""    
   full_name = first + ' ' + last    
   return full_name.title()
